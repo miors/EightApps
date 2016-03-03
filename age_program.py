b97dcb7839ba1a644d5bec87e5cbf8da950bf6bf
miorsufian <mior.sufian@bjss.com>
2016-03-03 10:28:43 +0000

    added new file

--- age_program.py
+++ age_program.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python
+
+import random
+
+while True:
+    user_input = input("Enter a number between 0 and 10: ")
+    magic_number = int(11 * random.random())
+    print("magic number is {}".format(magic_number))
+    if int(user_input) == magic_number:
+        print("You have won")
+        break
+    else:
+        print("Please pick another number")
